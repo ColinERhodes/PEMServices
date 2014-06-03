1de30f27471682d4f523c712bf75fad158c19109
OpenShift guest <colin.rhodes@ehealthtechnologies.com>
2014-06-03 12:28:54 -0400

    Added appointment

---
+++ our_modules/Appointments.js
@@ -0,0 +1,22 @@
+var mockAppointments = [
+    {
+        Location : 'Rhodes Hospital',
+        Date :'01/01/2013',
+        Provider : 'Colin Rhodes'
+    }
+];
+    
+var getAppointments = function(req,res,next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(200, mockAppointments);
+    return next();
+}
+
+var getAppointment = function(req,res,next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(200, mockAppointments[0]);
+    return next();
+}
+
+module.exports.getAppointments = getAppointments;
+module.exports.getAppointment = getAppointment;
