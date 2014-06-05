94e82689d2197073dc3059026e161e7f1d2f1395
OpenShift guest <colin.rhodes@ehealthtechnologies.com>
2014-06-05 16:32:31 -0400

    added new messages

--- our_modules/Messages.js
+++ our_modules/Messages.js
@@ -1,9 +1,14 @@
 var MessagesMock = [
     {
-         Text : "A new CT Angiogram is available",
-         Date : '1/1/2013',
-         From : "Colin Rhodes"
-        }
+        Text : "Memorial has send you a new cardiology note",
+        Date : '1/1/2012',
+        From : "eHealth Automated System"
+    },
+    {
+        Text : "RGH has sent you a new cardiac ultrasound",
+        Date : '1/1/2013',
+        From : "Colin Rhodes"
+    }   
     ];
     
 var getMessages = function(req,res,next) { 
